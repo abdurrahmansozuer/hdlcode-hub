dbbbce259e4c0e08da685ebd3cf9b91072d6a221
abdurrahman <abdurrahmansozuer@gmail.com>
2023-04-29 12:26:09 +0300

    new method about shifting bits on comment

--- toplevel.vhd
+++ toplevel.vhd
@@ -22,3 +22,25 @@ architecture slicing_with_rst of shift_reg_1_width is
     end process;
    
   end architecture slicing_with_rst;
+
+  -- architecture for_loop of shift_reg_1_width is
+ 
+  --   signal sr : std_logic_vector(sr_depth - 2 downto 0);
+   
+  -- begin
+   
+  --   process(clk)
+  --   begin
+  --     if rising_edge(clk) then
+   
+  --       for i in sr'high downto sr'low + 1 loop
+  --         sr(i) <= sr(i - 1);
+  --       end loop;
+   
+  --       sr(sr'low) <= sr_in;
+  --       sr_out <= sr(sr'high);
+   
+  --     end if;
+  --   end process;
+   
+  -- end architecture for_loop;
